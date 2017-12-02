f36183cc75f33af5c4a01a98c671fbd87d15f99c
rahul2240 <rahulsingh2240@gmail.com>
2017-12-03 00:40:56 +0530

    add gitapi

---
+++ app/controllers/gitapi_controller.rb
@@ -0,0 +1,26 @@
+class GitapiController < ApplicationController
+include HTTParty
+
+  def index
+
+        @a = Hash.new(0)
+
+        a = 1
+        stop = 0
+        while(stop==0)
+        @link = HTTParty.get('https://api.github.com/users/rahul2240/repos?page='+ a.to_s)
+        if @link.count<30
+          stop=1
+        end
+
+        @link.each do |lin|
+          if lin['language']!=nil
+            x = lin['language'].downcase
+
+            @a[x] += 1
+          end
+         end
+         a+=1
+       end
+  end
+end
---
+++ app/helpers/gitapi_helper.rb
@@ -0,0 +1,2 @@
+module GitapiHelper
+end
