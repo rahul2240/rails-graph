3a9fc5baa1d0d61ff58d84abaece536645ed857c
rahul2240 <rahulsingh2240@gmail.com>
2017-12-03 18:55:36 +0530

    Add codechef submissions

--- app/controllers/gitapi_controller.rb
+++ app/controllers/gitapi_controller.rb
@@ -1,26 +1,40 @@
 class GitapiController < ApplicationController
 include HTTParty
+require 'rubygems'
+require 'nokogiri'
+require 'rest-client'
 
   def index
+    @username = params[:username]
+        if @username
 
-        @a = Hash.new(0)
+            @language = Hash.new(0)
+            a = 1
+            stop = 0
+            while(stop==0)
+            @url = HTTParty.get('https://api.github.com/users/'+@username+'/repos?page='+ a.to_s)
+            if @url.count<30
+              stop=1
+            end
 
-        a = 1
-        stop = 0
-        while(stop==0)
-        @link = HTTParty.get('https://api.github.com/users/kunalvishnoi/repos?page='+ a.to_s)
-        if @link.count<30
-          stop=1
-        end
+            @url.each do |repo|
+              if repo['language']!=nil
+                x = repo['language'].downcase
+                @language[x] += 1
+              end
+             end
 
-        @link.each do |lin|
-          if lin['language']!=nil
-            x = lin['language'].downcase
-
-            @a[x] += 1
-          end
+             a+=1
+           end
          end
-         a+=1
        end
-  end
+
+      def codechef
+        @page = Nokogiri::HTML(RestClient.get("https://www.codechef.com/users/rahul2240"))
+        
+      end
+
+
+
+
 end
